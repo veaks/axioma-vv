0c446efe85d6c9de769619d191f7e2774b999cb2
Val <veaks@yandex.ru>
2021-08-15 23:17:36 +0300

    загрузка проекта_8

--- axioma/settings/production.py
+++ axioma/settings/production.py
@@ -17,7 +17,7 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
