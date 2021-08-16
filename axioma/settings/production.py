913bd5d35a23bfabfe7b29e6e5e380cd6783faa9
Val <veaks@yandex.ru>
2021-08-16 09:50:42 +0300

    dump data added

--- axioma/settings/production.py
+++ axioma/settings/production.py
@@ -27,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-his)e)tweviy=5d70e=262si$d)egk8tkd%9l4)0kbvm$h*a0s'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['axioma-vv.herokuapp.com']
+ALLOWED_HOSTS = ['axioma-vv.herokuapp.com',]
 
 
 # Application definition
